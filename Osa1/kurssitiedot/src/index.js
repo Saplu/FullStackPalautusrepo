bde0e350ff011c052ad003a7e52fbff04f044219
Halti <huhtala.h@hotmail.fi>
2020-03-22 16:31:22 +0200

    Osa1, tehtävä 1.5

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
     name: 'Fundamentals of React',
     exercises: 10
@@ -17,15 +18,17 @@ const App = () => {
     exercises: 14
     }
   ]
+}
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
+
 
 const Header = (props) => {
   return(
