d8f80af1485082d028a936517771c54d91585493
Halti Huhtala <huhtala.h@hotmail.fi>
2020-03-26 14:00:41 +0200

    Osa1, tehtävä 1.14

--- Osa1/anekdootit/src/index.js
+++ Osa1/anekdootit/src/index.js
@@ -5,6 +5,7 @@ const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = 
   useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0))
+  const [winner, setWinner] = useState(0)
 
   const NewRandom = () =>
     setSelected(GetRand(props.anecdotes.length))
@@ -13,15 +14,21 @@ const App = (props) => {
     const copy = [...points]
     copy[selected] += 1
     setPoints(copy)
+    let max = copy.indexOf(Math.max(...copy))
+    setWinner(max)
   }
 
 
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       <p>{props.anecdotes[selected]}</p>
       <p>Votes: {points[selected]}</p>
       <Button handleClick={Vote} text='vote'/>
       <Button handleClick={NewRandom} text='Random anecdote'/>
+      <h2>Anecdote with most votes</h2>
+      <p>{props.anecdotes[winner]}</p>
+      <p>Has {points[winner]} votes</p>
     </div>
   )
 }
