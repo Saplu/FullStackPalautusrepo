2404a9f1b0685d7e5ed2c145000687ea72f03bdd
Halti <huhtala.h@hotmail.fi>
2020-05-27 07:52:48 +0300

    Osa4, tehtävä 4.10

--- Osa4/tests/blog_api.test.js
+++ Osa4/tests/blog_api.test.js
@@ -27,6 +27,27 @@ test('id is correctly named', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'JSfulness',
+    author: 'pasi',
+    url: 'www.fi',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).toContain('JSfulness')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
