dcc79e78c46ce97301f87c38a67dfcd8691a9d39
Halti <huhtala.h@hotmail.fi>
2020-06-04 16:10:34 +0300

    Osa4, tehtävä 4.19

--- Osa4/controllers/blogs.js
+++ Osa4/controllers/blogs.js
@@ -1,22 +1,38 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  //const blogs = await Blog.find({})
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs.map(blog => blog.toJSON()))
 })
   
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  console.log(request.body)
   if (body.title === undefined || body.url === undefined){
     response.status(400).json('try better')
   }
 
   else{
-    const user = await User.findById(body.userId)
-    console.log(user)
+    const token = getTokenFrom(request)
+
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({error: 'token missing or invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id)
+    
     const blog = new Blog({
       title: body.title,
       author: body.author,
