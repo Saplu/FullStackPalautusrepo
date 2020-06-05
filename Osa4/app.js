1b19338c66a0a423e63d1190e4501a9826db6abc
Halti <huhtala.h@hotmail.fi>
2020-06-05 10:43:07 +0300

    Osa4, tehtävä 4.20

--- Osa4/app.js
+++ Osa4/app.js
@@ -8,6 +8,7 @@ const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 logger.info('connecting to ', config.MONGODB_URI)
 
@@ -21,6 +22,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
