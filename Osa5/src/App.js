2b9ae1cd4702fdd4e4dc3a2ecfc67dc297b24255
Halti <huhtala.h@hotmail.fi>
2020-06-07 15:45:07 +0300

    Osa5, tehtävä 5.2

--- Osa5/src/App.js
+++ Osa5/src/App.js
@@ -15,6 +15,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -22,12 +31,22 @@ const App = () => {
         username, password
       })
 
+      window.localStorage.setItem(
+        'loggedBlogUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch(exception) {
       console.log('wrong credentials')
     }
+  }
+
+  const logOut = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -60,7 +79,7 @@ const App = () => {
   const blogForm = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={logOut}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
