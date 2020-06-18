c8163751ef751553433b4342ac7d470859f7d1fe
Halti <huhtala.h@hotmail.fi>
2020-06-18 16:25:28 +0300

    Osa5, tehtävä 5.18

--- Osa5/cypress/integration/blogApp.spec.js
+++ Osa5/cypress/integration/blogApp.spec.js
@@ -1,11 +1,39 @@
+const { getByAltText } = require("@testing-library/react")
+
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Minä',
+      username: 'Saplu',
+      password: 'salasana'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.visit('http://localhost:3000')
     cy.contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('Saplu')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
+
+      cy.contains('Minä logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('Saplu')
+      cy.get('#password').type('salis')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
