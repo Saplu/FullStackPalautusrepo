ee2275133e009ace1e09a91396d367b33061b2f2
Halti Huhtala <huhtala.h@hotmail.fi>
2020-06-30 13:28:40 +0300

    Osa6, tehtävä 6.4

--- Osa6/anecdotes/src/reducers/anecdoteReducer.js
+++ Osa6/anecdotes/src/reducers/anecdoteReducer.js
@@ -24,25 +24,37 @@ const reducer = (state = initialState, action) => {
   console.log('action', action)
 
   switch(action.type) {
-  case 'VOTE':
-    const id = action.data.id
-    const anecdoteToVote = state.find(a => a.id === id)
-    const changedAnecdote = {
-      ...anecdoteToVote,
-      votes: anecdoteToVote.votes + 1
-    }
-    return state.map(anecdote =>
-      anecdote.id !== id ? anecdote : changedAnecdote
-    )
+    case 'VOTE':
+      const id = action.data.id
+      const anecdoteToVote = state.find(a => a.id === id)
+      const changedAnecdote = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
+    case 'ADD':
+      return [...state, action.data]
     default: return state
   }
-
 }
 
 export const vote = (id) => {
   return {
     type: 'VOTE',
     data: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'ADD',
+    data: {
+      content: content,
+      id: getId(),
+      votes: 0
+    }
   }
 }
 
