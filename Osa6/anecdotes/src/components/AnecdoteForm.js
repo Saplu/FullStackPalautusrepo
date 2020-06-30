97a12d1bdaf8f586d309722821246531efc2e4ed
Halti Huhtala <huhtala.h@hotmail.fi>
2020-06-30 13:52:48 +0300

    Osa6, tehtävät 6.6 ja 6.7

---
+++ Osa6/anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const NewAnecdote = (props) => {
+  const dispatch = useDispatch()
+
+  const AddAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    console.log(content)
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
+  }
+
+  return (
+    <form onSubmit={AddAnecdote}>
+      <div>
+        <input name="anecdote"/>
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+}
+
+export default NewAnecdote
