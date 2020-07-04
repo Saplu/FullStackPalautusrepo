73358813ffbc148a04418bef95d0279fbf03c61b
Halti Huhtala <huhtala.h@hotmail.fi>
2020-07-04 14:21:29 +0300

    Osa6, tehtävä 6.19

--- Osa6/anecdotes/src/components/AnecdoteList.js
+++ Osa6/anecdotes/src/components/AnecdoteList.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
-const Anecdotes = () => {
-  const anecdotes = useSelector(state => state.anecdotes)
-  const dispatch = useDispatch()
+const Anecdotes = (props) => {
 
   const AddVote = (id) => {
-    dispatch(vote(id))
-    const anecdote = anecdotes.find(a => a.id === id)
-    dispatch(setNotification(`You voted '${anecdote.content}'`, 5))
+    props.vote(id)
+    const anecdote = props.anecdotes.find(a => a.id === id)
+    props.setNotification(`You voted '${anecdote.content}'`, 5)
   }
 
   const sortAnecdotes = () => {
-    const sortedAnecdotes = [].concat(anecdotes).sort((a, b) => a.votes < b.votes ? 1 : -1)
+    const sortedAnecdotes = [].concat(props.anecdotes).sort((a, b) => a.votes < b.votes ? 1 : -1)
     return (
       sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
@@ -37,4 +35,16 @@ const Anecdotes = () => {
   )
 }
 
-export default Anecdotes
+const mapStateToProps = (state) => {
+  return {
+    anecdotes: state.anecdotes
+  }
+}
+
+const mapDispatchToProps = {
+  vote,
+  setNotification
+}
+
+const connectedAnecdotes = connect(mapStateToProps, mapDispatchToProps)(Anecdotes)
+export default connectedAnecdotes
