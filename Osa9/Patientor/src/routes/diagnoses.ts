cd8f504b4dc06d24f4217aa1c4323035ea9d81d1
Halti <huhtala.h@hotmail.fi>
2020-08-17 14:43:01 +0300

    Osa9, tehtävä 9.21

--- Osa9/Patientor/src/routes/diagnoses.ts
+++ Osa9/Patientor/src/routes/diagnoses.ts
@@ -4,9 +4,7 @@ import diagnoseService from '../services/diagnoseService';
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  console.log('diagnoses here');
   const data = diagnoseService.getEntries();
-  console.log(data);
   res.json(data);
 });
 
